39d2703592664295f37eb3e3f4af1465529b72be
kazuya <kazuya@kazuyanoMacBook-Pro.local>
2021-01-05 22:47:50 +0900

    会話ネタ登録画面の見た目修正

--- TalkStock/TalkStock/View/MemoCreate/MemoCreateView.swift
+++ TalkStock/TalkStock/View/MemoCreate/MemoCreateView.swift
@@ -22,45 +22,49 @@ struct MemoCreateView: View {
         NavigationView {
             
             VStack {
+                Spacer().frame(height: 60)
                 VStack(alignment: .leading) {
                     Group {
-                        Text("タイトル")
+                        Text("タイトル").font(.caption)
                         TextField("", text: $memoTitle)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                             .frame(width: UIComponents.screenWidth / 1.1, height: UIComponents.screenWidth / 12)
                         
-                        Text("タグ")
+                        Text("タグ").font(.caption)
                         TextField("", text: $memoTag)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                             .frame(width: UIComponents.screenWidth / 1.1, height: UIComponents.screenWidth / 12)
-                        Text("メモ")
+                        Text("メモ").font(.caption)
                         MultilineTextField(text: $memoText)
-                            .frame(width: UIComponents.screenWidth / 1.1, height: UIComponents.screenWidth / 2)
+                            .frame(width: UIComponents.screenWidth / 1.1, height: UIComponents.screenWidth / 2.5)
                             .border(Color(#colorLiteral(red: 0.7999121547, green: 0.8000506759, blue: 0.7999034524, alpha: 1)), width: 0.5)
+                            .cornerRadius(10)
                         
-                        Text("URL :")
+                        Text("URL :").font(.caption)
                         MultilineTextField(text: $memoUrl)
                             .frame(width: UIComponents.screenWidth / 1.1, height: UIComponents.screenWidth / 5)
                             .border(Color(#colorLiteral(red: 0.7999121547, green: 0.8000506759, blue: 0.7999034524, alpha: 1)), width: 0.5)
+                            .cornerRadius(10)
                     }
-                    
-                    Text("話したい人")
-                        .padding(.vertical)
-                    
-                    Button(action: {}) {
-                        Image(systemName: "plus.circle")
-                            .resizable()
-                            .frame(width: UIComponents.screenWidth / 10, height: UIComponents.screenWidth / 10)
-                            .foregroundColor(.black)
-                    }
+                    VStack(alignment: .leading) {
+                        Text("話したい人")
+                            .font(.caption)
+                        
+                        Button(action: {}) {
+                            Image(systemName: "plus.circle")
+                                .resizable()
+                                .frame(width: UIComponents.screenWidth / 10, height: UIComponents.screenWidth / 10)
+                                .foregroundColor(.black)
+                        }
+                    }.padding(.vertical, 20)
                 }
                 
                 Button(action: {}) {
                     Text("保存する")
-                        .frame(width: UIComponents.screenWidth / 1.5, height: UIComponents.screenWidth / 10)
+                        .frame(width: UIComponents.screenWidth - 50, height: UIComponents.screenWidth / 9)
                         .foregroundColor(.white)
                         .background(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
-                }
+                }.cornerRadius(10)
             }
             .frame(minWidth: 0,
                    maxWidth: .infinity,
